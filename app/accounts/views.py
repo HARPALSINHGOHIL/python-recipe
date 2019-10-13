66b56b90e91323992424aa39d196781eab27e1f5
Harpalsinh Gohil <harpalsinh.gohil4578@gmail.com>
2019-10-14 03:20:52 +0530

    Added messages init.

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 # Create your views here.
-def login(request):
-    return render(request, 'accounts/login.html')
-
-
 def register(request):
+    if request.method == 'POST':
+        messages.error(request, 'Testing Error message')
+        return redirect('register')
     return render(request, 'accounts/register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        print('SUBMITTED Login')
+        return redirect('login')
+    return render(request, 'accounts/login.html')
 
 
 def logout(request):
