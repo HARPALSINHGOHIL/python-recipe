d5fed99c215ad273a825dbe03cc75de2ee2ce15b
Harpalsinh Gohil <harpalsinh.gohil4578@gmail.com>
2019-10-13 21:01:32 +0530

    Added admin side panel improvements.

--- app/listings/admin.py
+++ app/listings/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Listing
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'sqft', 'list_date')
+    list_display_links = ('id', 'title')
+    list_editable = ('is_published',)
+    search_fields = ('title', 'address', 'city', 'state', 'zipcode', 'description')
+    list_filter = ('realtor', 'city', 'state')
+    list_per_page = 25
+
+
+admin.site.register(Listing, ListingAdmin)
--- app/realtors/admin.py
+++ app/realtors/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Realtor
 
-admin.site.register(Realtor)
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'phone', 'email', 'is_mvp', 'hire_date')
+    list_display_links = ('id','name',)
+    search_fields = ('name', 'phone', 'email')
+    list_filter = ('hire_date',)
+    list_per_page = 25
+
+
+admin.site.register(Realtor, RealtorAdmin)
